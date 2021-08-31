e71ca8c6eb753b6a0606aaaf8f3c55762d6423a1
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2021-08-31 11:20:05 +0200

    :ok_hands:

--- Formula/creekey.rb
+++ Formula/creekey.rb
@@ -5,7 +5,6 @@ class Creekey < Formula
   version "0.1.0-beta11"
   license ""
 
-
   head "https://github.com:opencreek/creekey-cli.git"
 
   depends_on "rust" => :build
@@ -17,33 +16,34 @@ class Creekey < Formula
     bin.install "target/release/creekey-git-sign"
   end
 
-  plist_options login: true
   def caveats
     "Run 'creekey pair' to pair with your phone!"
   end
 
-  def plist;
+  plist_options login: true
+
+  def plist
     <<-EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-        <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{bin}/creekey</string>
-        <string>agent</string>
-      </array>
-      <key>StandardOutPath</key>
-      <string>/opt/homebrew/var/log/homebrew/creekey.log</string>
-      <key>StandardErrorPath</key>
-      <string>/opt/homebrew/var/log/homebrew/creekey.log</string>
-      <key>KeepAlive</key>
-      <true/>
-    </dict>
-    </plist>
-  EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+          <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{bin}/creekey</string>
+          <string>agent</string>
+        </array>
+        <key>StandardOutPath</key>
+        <string>/opt/homebrew/var/log/homebrew/creekey.log</string>
+        <key>StandardErrorPath</key>
+        <string>/opt/homebrew/var/log/homebrew/creekey.log</string>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+      </plist>
+    EOS
   end
 
   test do
